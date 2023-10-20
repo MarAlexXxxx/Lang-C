0d31a3b05274545e6ebb952af344971315ec4fd7
MarAlex <huligan_90@inbox.ru>
2023-10-20 22:13:39 +0500

    Приветствие

---
+++ Example05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Лёха")
+{
+    Console.WriteLine("Здарова, Лёха!!!");
+}
+else
+{
+    Console.WriteLine("Ну привет...");
+    Console.WriteLine(username);
+}
